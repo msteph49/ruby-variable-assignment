df4aa2e76915872cfa78e758f0cff6462616471c
Michael Stephens <mdstephens26@yahoo.com>
2020-11-12 19:24:32 -0500

    commit tree e2b114b9f2b1e7298ba4de0cb6be80e7050c29c7

--- variable.rb
+++ variable.rb
@@ -1,2 +1,9 @@
- # Define a local variable called 'greeting',
+ # Define a local variable called 'greeting'
+
  # give it the value of the string "Hello World"
+
+ greeting = 'Hello World'
+ 
+
+ 
+
